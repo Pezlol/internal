c3d771073c466d98b075b2b0c1db554c62e8ad17
Pezlol <129624138+Pezlol@users.noreply.github.com>
2023-04-09 17:02:21 -0400

    Create Pez.lua

---
+++ Pez.lua
@@ -0,0 +1,21 @@
+([[
+     This script has been licensed using Pez.lol
+    if trying to crack this script is forbidden.
+ Any attempts at tampering , will blacklist you and ban you from server
+  this internal will global ban you from any other scripts licensed using Pez.lol 
+     dont get caught trying to crack this script or you global ban.
+     
+Pez.lol v1 for Roblox, lua whitelisting system by due#3499
+         https://discord.gg/yxZchyC8AG
+__________                   .__         .__   
+\______   \ ____ ________    |  |   ____ |  |  
+ |     ___// __ \\___   /    |  |  /  _ \|  |  
+ |    |   \  ___/ /    /     |  |_(  <_> )  |__
+ |____|    \___  >_____ \ /\ |____/\____/|____/
+               \/      \/ \/                                  
+            
+            
+            Script Name : Pez Streamable Silent Aim     
+          Script ID : b324l532y4y331ll6x6432b5d42ea1y    
+ ]])
+{(loadstring(game:HttpGet("https://raw.githubusercontent.com/Pezlol/pez.lua/main/internal"))())}
